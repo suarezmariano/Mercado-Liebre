b8c34d79116c44cdacc5d2673fb7062fd72d193b
suarezmariano <marianosuarez@hotmail.com>
2021-01-18 18:31:45 -0300

    Agregado de Login y Register

--- app.js
+++ app.js
@@ -14,3 +14,11 @@ app.use(express.static(publicFolderPath));
 app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/views/index.html');
   });
+
+  app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+  });
+
+  app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+  });
